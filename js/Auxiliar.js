78b94ab1304b23bd74613876f7349717b5d24395
Sergio Zúñiga Ramos <20201@CALASANZSALAMANCA.local>
2020-11-19 10:04:05 +0100

    js con comentarios

--- js/Auxiliar.js
+++ js/Auxiliar.js
@@ -1,23 +1,24 @@
 window.onload = function() {
     let contenidoVentana = window.opener;
     let cuerpo = document.getElementById("auxiliar");
-    cuerpo.innerHTML = contenidoVentana.enviarDatos();
+    cuerpo.innerHTML = contenidoVentana.enviarDatos();//añade la informacion del cuerpo
 
 
     let boton1 = document.getElementById("botonVolver");
     let boton2 = document.getElementById("botonTerminar");
 
-    boton1.addEventListener("click", cerrarVentana, false);
+    boton1.addEventListener("click", cerrarVentana, false);//añade la funcion cerrarVentaba
 
-    boton2.addEventListener("click", volverVentanaFruteria, false);
+    boton2.addEventListener("click", volverVentanaFruteria, false);//añade la funcion volverVentanaFruteria
 }
 
 
+//metodo que cierra la ventana
 function cerrarVentana() {
     window.close();
 }
 
-
+//metodo que cierra la ventana llama al metodo reiniciar y ejecuta el submit del formulario
 function volverVentanaFruteria() {
     let contenidoVentana = window.opener;
 
--- js/funciones.js
+++ js/funciones.js
@@ -1,38 +1,4 @@
-var precioFruta = new Array(0.80, 1.20, 2, 3.60, 1.10, 3.40, 6, 0.90, 1.60, 2.10);
-var proximidadFruta = new Array(true, true, false, true, false, false, true, false, true, false);
-var regionFruta = new Array("Canarias", "Leon", "Zamora", "Avila", "Caceres", "Barcelona", "Madrid", "Galicia", "Valencia", "Albacete");
-var conservaFruta = new Array(false, true, true, true, true, false, true, false, false, true);
-var nombrefruta = new Array("Platano", "Manzana", "Pera", "Sandia", "Melon", "Kiwi", "Naranja", "Pomelo", "Melocoton", "Cereza");
-
-
-var almacenObjeto = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-var cantidadFruta = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-var precioTotal = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-
-
-class Fruta {
-    constructor(nombre, precio) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = 0;
-    }
-}
-
-class FrutaVerano extends Fruta {
-    constructor(nombre, precio, proximidad, region) {
-        super(nombre, precio);
-        this.proximidad = proximidad;
-        this.region = region;
-    }
-}
-
-class FrutaInvierno extends Fruta {
-    constructor(nombre, precio, conserva) {
-        super(nombre, precio);
-        this.conserva = conserva;
-    }
-}
-
+//todas las variables auxiliares que se va a utilizar en el ejercicio
 var inputs;
 var total = 0;
 var aux = 0;
@@ -47,6 +13,43 @@ var codigo;
 var spans;
 var ventaAux;
 
+//array que guardaran la informacion de los objetos, como los objetos
+var precioFruta = new Array(0.80, 1.20, 2, 3.60, 1.10, 3.40, 6, 0.90, 1.60, 2.10);
+var proximidadFruta = new Array(true, true, false, true, false, false, true, false, true, false);
+var regionFruta = new Array("Canarias", "Leon", "Zamora", "Avila", "Caceres", "Barcelona", "Madrid", "Galicia", "Valencia", "Albacete");
+var conservaFruta = new Array(false, true, true, true, true, false, true, false, false, true);
+var nombrefruta = new Array("Platano", "Manzana", "Pera", "Sandia", "Melon", "Kiwi", "Naranja", "Pomelo", "Melocoton", "Cereza");
+var almacenObjeto = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+var cantidadFruta = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+var precioTotal = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+//clase fruta
+class Fruta {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = 0;
+    }
+}
+
+//clase fruta verano que hereda de fruta
+class FrutaVerano extends Fruta {
+    constructor(nombre, precio, proximidad, region) {
+        super(nombre, precio);
+        this.proximidad = proximidad;
+        this.region = region;
+    }
+}
+//clase fruta invierno que hereda de fruta
+class FrutaInvierno extends Fruta {
+    constructor(nombre, precio, conserva) {
+        super(nombre, precio);
+        this.conserva = conserva;
+    }
+}
+
+
+
 window.onload = function() {
     
     inputs = document.getElementsByTagName("input");
@@ -55,15 +58,15 @@ window.onload = function() {
     imagenes = document.getElementsByTagName("img");
     codigo = document.getElementById("parrafoCodigo");
     spans = document.getElementsByTagName("span");
-    abrirVentana();
-    añadirEventosImagenes();
-    añadirEventosRadio();
-    añadirSpan();
+    abrirVentana();//metodo que verifica el formulario y abre si es necesario la ventana auxiliar
+    añadirEventosImagenes();//añade a las imagenes el evento click
+    añadirEventosRadio();//añade a los radio buton el evento para que aparezca el codigo
+    añadirSpan();//añade a los spans la informacion de la fruta
 }
 
 
 function añadirEventosImagenes() {
-
+   
     for (let index = 0; index < imagenes.length; index++) {
         imagenes[index].addEventListener("click", almacen.bind(index));
     }
@@ -82,7 +85,7 @@ function añadirSpan() {
     }
 }
 
-
+//metodo que genera el objeto de la fruta dependiendo de la imagen clicada, le añadimos informacion y lo añadimos a un array
 function almacen(num) {
     num = this.toString();
     for (var i = 0; i < 10; i++) {
@@ -102,7 +105,7 @@ function almacen(num) {
                 almacenObjeto[i] = frutaI;
 
             }
-            cantidad(num);
+            cantidad(num);//metodo que verifica si los kilos se han añadido correctamento con el formato correcto
             precioTotal[i] = almacenObjeto[i].cantidad * almacenObjeto[i].precio;
         }
     }
@@ -121,6 +124,7 @@ function cantidad(num) {
 
 }
 
+//comprueba que radio button esta clicado y visualiza o no el input del codigo
 function comprobarCodigo() {
     var inputCo = document.getElementById("inputCodigo");
     if (radioTarjeta1.checked == true) {
@@ -133,6 +137,7 @@ function comprobarCodigo() {
     }
 }
 
+//metodo que añade la informacion necesaria al div lateral, como el color a todos los parrafos de las mismas frutas
 function lateral(num, nombre) {
     divLateral = document.getElementById("lateral");
     divLateral.scrollIntoView(false);
@@ -154,7 +159,7 @@ function lateral(num, nombre) {
     }
 
 }
-
+//metodo que recoge la fecha, toda la informacion de cada fruta asi como el precio total y media. 
 function enviarDatos() {
     var textoVentana = "";
     if (permitir) {
@@ -180,6 +185,7 @@ function enviarDatos() {
 
 }
 
+//metodo que se le añade a los spans la informacion de la fruta (proximidad y region)
 function ventanaInfo(i) {
     var texto = "";
     if (i % 2 == 0) {
@@ -200,7 +206,7 @@ function ventanaInfo(i) {
 }
 
 
-
+//metodo que  vacia el div ateral como las variables que se han utilizado
 function reiniciar() {
     divLateral.innerHTML = "";
 
@@ -217,7 +223,7 @@ function reiniciar() {
     colores = "";
 }
 
-
+//metodo que verifica los campos del formulario y abre la ventana y es necesario
 function abrirVentana() {
     var nombreI = document.getElementById("inputNombre");
     var apellidosI = document.getElementById("inputApellidos");
